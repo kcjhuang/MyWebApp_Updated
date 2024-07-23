ed9bb708794e8992b727ec31a8212b30c0cca29c
phil <phil_jhuang@wiwynn.com>
2024-07-23 21:30:53 +0800

    UPDATE4

--- Startup.cs
+++ Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Web;
 
 public class Startup
 {
@@ -26,9 +25,7 @@ public class Startup
         {
             endpoints.MapGet("/", async context =>
             {
-                string url = "/【不斷更新】北北基桃明天停班課 全台颱風假一覽.html";
-                string encodedUrl = HttpUtility.UrlPathEncode(url);
-                context.Response.Redirect(encodedUrl);
+                await context.Response.SendFileAsync("wwwroot/index.html");
             });
         });
     }
